f4425dc786eecab45830b91ee8207ab198379003
Riley Flynn <nint8835@gmail.com>
2016-03-18 15:27:44 -0230

    Updated version number.

--- NintbotForDiscord/Bot.py
+++ NintbotForDiscord/Bot.py
@@ -14,7 +14,7 @@ __author__ = 'Riley Flynn (nint8835)'
 
 
 class Bot(TokenClient):
-    VERSION = "03.13.2016.2"
+    VERSION = "03.18.2016.1"
 
     def __init__(self, config: dict, loop: asyncio.BaseEventLoop = None):
         super(Bot, self).__init__(loop = loop)
