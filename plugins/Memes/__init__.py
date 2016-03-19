66e383a87a1d7d3f61876ea5a0b860228a866d92
Riley Flynn <riley@rileyflynn.me>
2016-03-19 15:14:19 -0230

    Created a new plugin called "Memes" - Includes command meme to get a random submission from a list of subreddits.

---
+++ plugins/Memes/__init__.py
@@ -0,0 +1,31 @@
+from NintbotForDiscord.Enums import EventTypes
+from NintbotForDiscord.Plugin import BasePlugin
+from NintbotForDiscord.Permissions import Permission
+
+import praw
+
+__author__ = 'Riley Flynn (nint8835)'
+
+
+subreddits = ["memes",
+              "dankmemes",
+              "funnymeme",
+              "meme",
+              "terriblefacebookmemes"]
+
+
+class Plugin(BasePlugin):
+
+    def __init__(self, bot_instance, plugin_data, folder):
+        super(Plugin, self).__init__(bot_instance, plugin_data, folder)
+        self.praw = praw.Reddit(user_agent = "Memes plugin V{} for NintbotForDiscord. Developed by /u/nint8835".format(plugin_data["plugin_version"]))
+        self.bot.CommandRegistry.register_command("meme",
+                                                  "Posts an amazing meme.",
+                                                  Permission(),
+                                                  plugin_data)
+        self.bot.register_handler(EventTypes.CommandSent, self.on_command, self)
+
+    async def on_command(self, args):
+        if args["command_args"][0] == "meme":
+            submission = self.praw.get_random_submission("+".join(subreddits))
+            await self.bot.send_message(args["channel"], submission.url)
