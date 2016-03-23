f9aec4e50f39b7ff5a1781cd9b78ac0fa1b49fb0
Riley Flynn <riley@rileyflynn.me>
2016-03-23 17:36:25 -0230

    Updated Discord Markov to use the new handler system

--- plugins/DiscordMarkov/__init__.py
+++ plugins/DiscordMarkov/__init__.py
@@ -23,23 +23,26 @@ class Plugin(BasePlugin):
         self.cleanstrings_permission_group = create_match_any_permission_group([ManageMessages(), Owner(self.bot)])
         logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(logging.ERROR)
         self.bot.register_handler(EventTypes.Message, self.on_message, self)
-        self.bot.register_handler(EventTypes.CommandSent, self.on_command, self)
         self.bot.CommandRegistry.register_command("wisdom",
                                                   "Generates wisdom using markov chains.",
                                                   Permission(),
-                                                  plugin_data)
+                                                  plugin_data,
+                                                  self.command_wisdom)
         self.bot.CommandRegistry.register_command("cleanstrings",
                                                   "Cleans extra data from the strings used to generate data.",
                                                   self.cleanstrings_permission_group,
-                                                  plugin_data)
+                                                  plugin_data,
+                                                  self.command_cleanstrings)
         self.bot.CommandRegistry.register_command("web_learn",
                                                   "Learns messages from a plain text file hosted online.",
                                                   Owner(self.bot),
-                                                  plugin_data)
+                                                  plugin_data,
+                                                  self.command_web_learn)
         self.bot.CommandRegistry.register_command("messages",
                                                   "Gives the number of messages the bot has to base it's knowledge off of.",
                                                   Permission(),
-                                                  plugin_data)
+                                                  plugin_data,
+                                                  self.command_messages)
         self.strings = []
         self.load_strings()
         self.chain = pymarkovchain.MarkovChain()
@@ -63,8 +66,8 @@ class Plugin(BasePlugin):
         self.strings.append(args["message"].content)
         self.save_strings()
 
-    async def on_command(self, args):
-        if args["command_args"][0] == "wisdom" and len(args["command_args"]) == 1:
+    async def command_wisdom(self, args):
+        if len(args["command_args"]) == 1:
             await self.bot.send_typing(args["channel"])
             try:
                 message_string_task = self.bot.EventManager.loop.run_in_executor(None, self.chain.generateString)
@@ -85,18 +88,7 @@ class Plugin(BasePlugin):
                 await self.bot.send_message(args["channel"], message_string)
             except:
                 await self.bot.send_message(args["channel"], "```{}```".format(traceback.format_exc(2)))
-
-        elif args["command_args"][0] == "cleanstrings" and self.cleanstrings_permission_group.has_permission(args["author"]):
-            count = 0
-            for item in self.strings[:]:
-                if item == "" or item.startswith("http") or item.startswith("~") or item.count("its ok to accept urself nintbot") >= 1 or item.count("Nathanial is the") >= 1:
-                    count += 1
-                    self.strings.remove(item)
-
-            self.save_strings()
-            await self.bot.send_message(args["channel"], "Removed {} extra strings.".format(count))
-
-        if args["command_args"][0] == "wisdom" and len(args["command_args"]) >= 2:
+        elif len(args["command_args"]) >= 2:
             await self.bot.send_typing(args["channel"])
             try:
                 message_string = ""
@@ -123,10 +115,21 @@ class Plugin(BasePlugin):
             except:
                 await self.bot.send_message(args["channel"], "```{}```".format(traceback.format_exc(2)))
 
-        if args["command_args"][0] == "web_learn" and len(args["command_args"])==2 and Owner(self.bot).has_permission(args["member"]):
+    async def command_cleanstrings(self, args):
+        count = 0
+        for item in self.strings[:]:
+            if item == "" or item.startswith("http") or item.startswith("~") or item.count("its ok to accept urself nintbot") >= 1 or item.count("Nathanial is the") >= 1:
+                count += 1
+                self.strings.remove(item)
+
+        self.save_strings()
+        await self.bot.send_message(args["channel"], "Removed {} extra strings.".format(count))
+
+    async def command_web_learn(self, args):
+        if len(args["command_args"])==2:
             data = requests.get(args["command_args"][1]).text
             self.strings.append(data)
             self.save_strings()
 
-        if args["command_args"][0] == "messages":
-            await self.bot.send_message(args["channel"], "The bot knows {} messages.".format(len(self.strings)))
+    async def command_messages(self, args):
+        await self.bot.send_message(args["channel"], "The bot knows {} messages.".format(len(self.strings)))
