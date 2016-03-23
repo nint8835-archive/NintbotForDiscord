caaa958b8d286c72c65f70529cbbda1721a006f3
Riley Flynn <riley@rileyflynn.me>
2016-03-23 17:04:39 -0230

    Updated Quotes to use new handler system

--- plugins/Quotes/__init__.py
+++ plugins/Quotes/__init__.py
@@ -21,27 +21,26 @@ class Plugin(BasePlugin):
         self.bot.CommandRegistry.register_command("quote",
                                                   "View and manage quotes.",
                                                   Permission(),
-                                                  plugin_data)
-        self.bot.register_handler(EventTypes.CommandSent, self.on_command, self)
+                                                  plugin_data,
+                                                  self.on_command)
         sys.path.append(os.path.join(folder))
         from JSONDB import JSONDatabase, SelectionMode
         global JSONDatabase, SelectionMode
         self.quotes = JSONDatabase(os.path.join(self.folder, "quotes.json"))
 
     async def on_command(self, args):
-        if args["command_args"][0] == "quote":
-            if len(args["command_args"])>=4:
-                if args["command_args"][1] == "add" and self.admin.has_permission(args["author"]):
-                    try:
-                        quote_msg = args["command_args"][2]
-                        quote_author = " ".join(args["command_args"][3:])
-                        quote_addtime = datetime.datetime.now().strftime("%x %X")
-                        self.quotes.insert({"msg": quote_msg,
-                                            "author": quote_author,
-                                            "added_at": quote_addtime})
-                        await self.bot.send_message(args["channel"], ":ballot_box_with_check: New quote added to database.")
-                    except:
-                        traceback.print_exc(5)
-            if len(args["command_args"]) == 1:
-                quote = random.choice(self.quotes.select(SelectionMode.ALL).rows)
-                await self.bot.send_message(args["channel"], "\"{}\" {}".format(quote["msg"], quote["author"]))
+        if len(args["command_args"])>=4:
+            if args["command_args"][1] == "add" and self.admin.has_permission(args["author"]):
+                try:
+                    quote_msg = args["command_args"][2]
+                    quote_author = " ".join(args["command_args"][3:])
+                    quote_addtime = datetime.datetime.now().strftime("%x %X")
+                    self.quotes.insert({"msg": quote_msg,
+                                        "author": quote_author,
+                                        "added_at": quote_addtime})
+                    await self.bot.send_message(args["channel"], ":ballot_box_with_check: New quote added to database.")
+                except:
+                    traceback.print_exc(5)
+        if len(args["command_args"]) == 1:
+            quote = random.choice(self.quotes.select(SelectionMode.ALL).rows)
+            await self.bot.send_message(args["channel"], "\"{}\" {}".format(quote["msg"], quote["author"]))
