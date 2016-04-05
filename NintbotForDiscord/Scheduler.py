7f48fa7f99a7452b3ce77977e8ca4f9161bfa402
Riley Flynn <riley@rileyflynn.me>
2016-04-05 19:19:03 -0230

    The massive documentation update: part 4

--- NintbotForDiscord/Scheduler.py
+++ NintbotForDiscord/Scheduler.py
@@ -13,6 +13,9 @@ class Scheduler:
         self.bot.EventManager.loop.create_task(self.handle_tasks())
 
     async def handle_tasks(self):
+        """
+        The loop that checks and executes tasks
+        """
         while not self.bot.is_closed:
 
             for task in self.tasks[:]:
@@ -23,10 +26,19 @@ class Scheduler:
             await asyncio.sleep(1)
 
     def add_task(self, task_instance: ScheduledTask, plugin_info: dict):
+        """
+        Adds a new task to the task list
+        :param task_instance: The task to add
+        :param plugin_info: The plugin_data dictionary of the plugin that is adding the task
+        """
         self.tasks.append({"task": task_instance,
                            "plugin": plugin_info})
 
     def remove_tasks_for_plugin(self, plugin_info: dict):
+        """
+        Removes all tasks for a plugin
+        :param plugin_info: The plugin_data dictionary of the plugin that is adding the task
+        """
         for task in self.tasks[:]:
             if task["plugin"] == plugin_info:
                 self.tasks.remove(plugin_info)
--- NintbotForDiscord/__init__.py
+++ NintbotForDiscord/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'Riley Flynn (nint8835)'
-__version__ = "04.05.2016.1"
+__version__ = "04.05.2016.2"
