28a78ff03f944937615c1fc59a55d42591b6039e
Riley Flynn <riley@rileyflynn.me>
2016-05-01 12:30:10 -0230

    Finished adding message repeating tasks to the TaskScheduler plugin

--- plugins/TaskScheduler/__init__.py
+++ plugins/TaskScheduler/__init__.py
@@ -1,9 +1,12 @@
 import os
 import json
+import traceback
 
 from discord import Object
 
+from NintbotForDiscord.Permissions import create_match_any_permission_group
 from NintbotForDiscord.Permissions.Special import Owner
+from NintbotForDiscord.Permissions.Text import ManageMessages
 from NintbotForDiscord.Plugin import BasePlugin
 from plugins.TaskScheduler.CustomTasks import ScheduledMessage, RepeatingScheduledMessage
 
@@ -20,7 +23,6 @@ class Plugin(BasePlugin):
                 self.tasks = json.load(f)
         except FileNotFoundError:
             self.tasks = []
-            # self.task_objs
             self.save_tasks()
 
         self.bot.CommandRegistry.register_command("test_task",
@@ -29,11 +31,20 @@ class Plugin(BasePlugin):
                                                   plugin_data,
                                                   self.command_testtask)
 
+        self.bot.CommandRegistry.register_command("task",
+                                                  "Manages scheduled tasks.",
+                                                  create_match_any_permission_group([Owner(self.bot), ManageMessages()]),
+                                                  plugin_data,
+                                                  self.command_task)
+
+        self.add_tasks()
+
     def save_tasks(self):
         with open(os.path.join(self.folder, "tasks.json"), "w") as f:
             json.dump(self.tasks, f)
 
     def add_tasks(self):
+        self.bot.Scheduler.remove_tasks_for_plugin(self.plugin_data)
         for task in self.tasks:
             if task["type"] == "message":
                 self.bot.Scheduler.add_task(ScheduledMessage(Object(task["destination"]),
@@ -57,3 +68,30 @@ class Plugin(BasePlugin):
                            "message": "I'm a scheduled message. Hello!",
                            "delay": 15})
         self.add_tasks()
+
+    async def command_task(self, args):
+        try:
+            if args["command_args"][1] == "add":
+                delay = int(args["command_args"][3])
+
+                if args["command_args"][2] == "message-repeating":
+                    self.tasks.append({"type": "message-repeating",
+                                       "destination": args["channel"].id,
+                                       "message": args["command_args"][4],
+                                       "delay": delay})
+                    await self.bot.send_message(args["channel"], ":ballot_box_with_check: Task added.")
+                    self.save_tasks()
+                    self.add_tasks()
+
+            elif args["command_args"][1] == "list":
+                await self.bot.send_message(args["channel"], "```{}```".format("\n".join(
+                    ["{}:\n{}".format(index + 1, json.dumps(item, sort_keys=True, indent=4)) for index, item in enumerate(self.tasks)]
+                )))
+
+            elif args["command_args"][1] == "remove":
+                self.tasks.remove(self.tasks[int(args["command_args"][2]) - 1])
+                await self.bot.send_message(args["channel"], ":ballot_box_with_check: Task removed.")
+                self.save_tasks()
+                self.add_tasks()
+        except:
+            traceback.print_exc(5)
